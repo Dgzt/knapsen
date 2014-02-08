129bfe59ee34a94b603ea53ab0552d2364fa7321
Zsuró Tibor <zsurotibor@gmail.com>
2014-02-08 13:07:07 +0100

    The test of Card class is complete.

--- test/engine/cardtest.cpp
+++ test/engine/cardtest.cpp
@@ -3,8 +3,60 @@
 #include "cardtest.h"
 
 void TestCard::testValue(){
-  Card *testCard = new Card(1);
-  QCOMPARE( testCard->getValue(), 1 );
+  int cardValue = 1;
+  
+  Card *testCard = new Card( cardValue );
+  QCOMPARE( testCard->getValue(), cardValue );
+}
+
+void TestCard::testSelectable(){
+  int cardValue = 1;
+  
+  Card *testCard = new Card( cardValue );
+  testCard->setSelectable( true );
+  
+  QVERIFY( testCard->isSelectable() );
+  
+  testCard->setSelectable( false );
+  
+  QVERIFY( !testCard->isSelectable() );
+}
+
+void TestCard::testCardText(){
+  int cardValue = 5;
+  QString cardText("1_diamond");
+  
+  Card *testCard = new Card( cardValue );
+
+  QCOMPARE( testCard->getCardText(), cardText );
+}
+
+void TestCard::testCardPoint()
+{
+  int cardValue = 3;
+  int cardPoint = 2;
+  
+  Card *testCard = new Card( cardValue );
+  
+  QCOMPARE( testCard->getCardPoint(), cardPoint );
+}
+
+void TestCard::testCardSuit()
+{
+  int cardValue = 11;
+  Card::Suit cardSuit = Card::Spade;
+  
+  Card *testCard = new Card( cardValue );
+  QCOMPARE( testCard->getSuit(), cardSuit );
+}
+
+void TestCard::testCardType()
+{
+  int cardValue = 21;
+  Card::Type cardType = Card::Nine;
+  
+  Card *testCard = new Card( cardValue );
+  QCOMPARE( testCard->getType(), cardType );
 }
 
 QTEST_MAIN( TestCard )
--- test/engine/cardtest.h
+++ test/engine/cardtest.h
@@ -1,3 +1,27 @@
+/*!
+ * @file
+ * @author  Zsuro Tibor <zsurotibor@gmail.com>
+ * 
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ * The TestCard class test the Card class.
+ */
+
 #ifndef TESTCARD_H
 #define TESTCARD_H
 #include <QtCore>
@@ -8,7 +32,35 @@ class TestCard : public QObject
   Q_OBJECT
   
 private slots:
+  /*!
+   * Test the 'getValue' function.
+   */
   void testValue();
+  
+  /*!
+   * Test the 'setSelectable' and 'isSelectable' functions.
+   */
+  void testSelectable();
+  
+  /*!
+   * Test the 'getCardText' function.
+   */
+  void testCardText();
+  
+  /*!
+   * Test the 'getCardPoint' function.
+   */
+  void testCardPoint();
+  
+  /*!
+   * Test the 'getCardSuit' function.
+   */
+  void testCardSuit();
+  
+  /*!
+   * Test the 'getCardType' function.
+   */
+  void testCardType();
   
 };
 
